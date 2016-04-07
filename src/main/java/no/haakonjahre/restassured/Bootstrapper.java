1cc233cbd308180fe9904a6a051370dd6a56059f
haakobja <haakobja@gmail.com>
2016-04-07 16:28:32 +0200

    Added webapp bootstrap

---
+++ src/main/java/no/haakonjahre/restassured/Bootstrapper.java
@@ -0,0 +1,8 @@
+package no.haakonjahre.restassured;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class Bootstrapper extends Application {
+}
